c13f2a0c13b79174a4e53caf53151d9faeef8c8a
Bilal Babale <bilalbabale@gmail.com>
2020-09-29 10:38:28 +0100

    feat: Add function to convert given string to integers

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -3,6 +3,13 @@
 def lowest_terms(x):
 	return ""
 
+def string_to_nums(string):
+	splitted_strings = string.split('/')
+	splitted_nums = [int(string) for string in splitted_strings]
+	return splitted_nums
 
 if __name__ == "__main__":
-	pass
+	num, denom = string_to_nums('2/3')
+	print(num)
+	print(denom)
+	print (num + denom)
