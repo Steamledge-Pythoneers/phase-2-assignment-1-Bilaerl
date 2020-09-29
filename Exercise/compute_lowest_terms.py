b2a3d7f1d9c3139c086807c5bbbd76660405730c
Bilal Babale <bilalbabale@gmail.com>
2020-09-29 12:52:37 +0100

    feat: Add function to find highest possible common factor of the given fraction

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,15 +1,37 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	return ""
+	num, denom = string_to_nums(x)
+	hpcf = highest_possible_common_factor(num,denom)
+
+	print(num)
+	print(denom)
+	print(hpcf)
+	#return ""
+
 
 def string_to_nums(string):
 	splitted_strings = string.split('/')
 	splitted_nums = [int(string) for string in splitted_strings]
 	return splitted_nums
 
+
+def highest_possible_common_factor(num,denom):
+	#find the smallest between num and denom
+	smallest = abs(num) if abs(num) < abs(denom) else abs(denom)
+	
+	if smallest == 1:
+		hpcf = 1
+	elif smallest == 2:
+		hpcf = 2
+	elif smallest == 3:
+		hpcf = 3
+	else:
+		hpcf = smallest//2
+
+	#return highest possible common factor
+	return hpcf
+
+
 if __name__ == "__main__":
-	num, denom = string_to_nums('2/3')
-	print(num)
-	print(denom)
-	print (num + denom)
+	lowest_terms('15/10')
